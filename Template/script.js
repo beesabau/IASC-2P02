bcdf471bbbf48a4a847193638ef735d87ccb30b1
beesabau <qa22xw@brocku.ca>
2025-01-24 12:24:04 -0500

    Added Template and Prototype 1 pages.

---
+++ Template/script.js
@@ -0,0 +1,61 @@
+//console.log("Hello world!")
+import * as THREE from 'three'
+
+//console.log(THREE)
+
+//CANVAS
+const canvas = document.querySelector(".webgl")
+
+//SCENE
+const scene = new THREE.Scene()
+scene.background = new THREE.Color("pink")
+
+//CAMERA
+const camera = new THREE.PerspectiveCamera(
+    75, //field of view
+    window.innerWidth / window.innerHeight, //aspect ratio
+    0.1, //how near can the camera see
+    100 //how far can the camera see
+)
+scene.add(camera)
+camera.position.set(0, 0, 5)
+//RENDERER
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+//MESHES
+
+//testSphere
+
+const sphereGeometry = new THREE.SphereGeometry(1)
+const sphereMaterial = new THREE.MeshNormalMaterial()
+const testSphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+
+scene.add(testSphere)
+testSphere.position.set(0, 0, 0)
+
+//ANIMATION LOOP
+
+const clock = new THREE.Clock()
+const animation = () =>
+{
+    //return elapsedTime
+    const elapsedTime = clock.getElapsedTime()
+
+    //renderer
+    renderer.render(scene, camera)
+
+    //request next frame
+    window.requestAnimationFrame(animation)
+}
+animation()
+
+/* const myFirstFunction = () =>
+{
+    console.log("Function")
+}
+
+myFirstFunction() */
